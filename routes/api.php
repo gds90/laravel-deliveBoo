656a3e6da392992a8dc76e534e715d46edf89952
gds90 <you@example.com>
2024-03-26 16:37:34 +0100

    Starting work on ApiControllers

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\TypeController;
+use App\Http\Controllers\Api\DishController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/restaurant', [RestaurantController::class, 'index']);
+Route::get('/restaurant/{slug}', [RestaurantController::class, 'show']);
+Route::get('/restaurant/type/{slug}', [RestaurantController::class, 'get_type_restaurants']);
+
+Route::get('/type', [TypeController::class, 'index']);
+
+// Route::post('/contacts', [LeadController::class, 'store']);
