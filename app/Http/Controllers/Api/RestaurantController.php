3f41aa8344c7aa096cd1fafae4f6627ec3e815e4
gds90 <you@example.com>
2024-03-26 17:29:32 +0100

    Created Restaurant Type Api Controller

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -4,8 +4,41 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Restaurant;
+use App\Models\Type;
 
 class RestaurantController extends Controller
 {
-    //
+    public function index()
+    {
+        $restaurants = Restaurant::with('types')->paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'results' => $restaurants
+        ]);
+    }
+
+    public function show($slug)
+    {
+        //
+    }
+
+    public function get_type_restaurants($slug)
+    {
+        // cerco la tipologia di ristorante corrispondente allo slug
+        $type = Type::where('slug', $slug)->first();
+
+        // recupero i ristoranti che hanno la tipologia scelta
+        $restaurants = Restaurant::whereHas('types', function ($query) use ($type) {
+            $query->where('id', $type->id);
+        })->get();
+
+        return response()->json([
+            'success' => true,
+            'results' => $restaurants
+        ]);
+    }
 }
